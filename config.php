3d275d5ab7ad0c610128d4fc4adc4c68b2f264e8
DexsDevelopers <empresatokio@gmail.com>
2026-03-01 21:02:40 -0300

    Migrated PixGo API Key from site-rastreamento project

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ define('DB_PASS', 'Lucastav8012@');
 define('DB_NAME', 'u853242961_pixgo');
 
 // Configurações do PixGo.org
-define('PIXGO_API_KEY', 'SUA_API_KEY_AQUI');
-define('PIXGO_PROJECT_ID', 'ID_DO_SEU_PROJETO'); // Se necessário pela API
+define('PIXGO_API_KEY', 'pk_c8e1450b597c3a1cd95b5e6856ca2dde7637a7fbe8abde58ae3eadb06135f7d6');
+define('PIXGO_PROJECT_ID', ''); // Não encontrado no projeto anterior, opcional
 
 // Configurações Gerais
 define('DEFAULT_LIQUID_ADDRESS', 'lq1q2w3e4r5t6y7u8i9o0p...'); // Endereço padrão se o usuário não definir
